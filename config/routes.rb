188023f3344f1a0bbe0494bfc1f3531ec41204f1
Akulov Andrey <andreyshark@gmail.com>
2015-03-10 07:40:44 +0300

    Add missed context in routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   resources :videos
 
-  root to: 'videos#index', as: :authenticated_root
+  authenticated :user do
+    root :to => 'videos#index', as: :authenticated_root
+  end
+
   root to: 'visitors#index'
 
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
