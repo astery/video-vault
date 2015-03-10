ea7d38657ed4590d63bd09949c1a841be195f82c
Akulov Andrey <andreyshark@gmail.com>
2015-03-10 05:44:15 +0300

    Add video fields requirement validation. Add error output while video is invalid.

--- app/models/video.rb
+++ app/models/video.rb
@@ -7,5 +7,7 @@ class Video < ActiveRecord::Base
   validates_attachment_content_type :file, :content_type => /\Avideo\/.*\Z/
   process_in_background :file
 
+  validates :title, :show_at, :file, presence: true
+
   scope :need_to_be_scheduled_for, -> (interval) { where(show_at: Time.now..(Time.now + interval)) }
 end
