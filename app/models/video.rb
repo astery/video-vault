f6d023759c649ce6e06cd385eb47f7e646a6edfa
Akulov Andrey <andreyshark@gmail.com>
2015-03-10 05:00:11 +0300

    Change paperclip video styles resolutions.

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,8 +1,8 @@
 class Video < ActiveRecord::Base
   belongs_to :user
   has_attached_file :file, :styles => {
-    main: { :geometry => "400x300", :format => 'mp4', :streaming => true },
-    thumb: { :geometry => "100x100#", :format => 'jpg', :time => 5 }
+    main: { :geometry => "640x480", :format => 'mp4', :streaming => true },
+    thumb: { :geometry => "120x90#", :format => 'jpg', :time => 5 }
   }, processors: [:ffmpeg, :qtfaststart]
   validates_attachment_content_type :file, :content_type => /\Avideo\/.*\Z/
   process_in_background :file
